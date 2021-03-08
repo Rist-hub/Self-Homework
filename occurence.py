7ea95a4063ecf3cb068f10135c7733ee11fa989c
Rist-hub <58072640+Rist-hub@users.noreply.github.com>
2021-03-08 18:14:43 +0530

    Update occurence.py

--- occurence.py
+++ occurence.py
@@ -7,8 +7,10 @@ def occurence(letter_string):
             c=c+1
     if len(_list)>1:
         print("letter",letter,"from the given string {",letter_string,"} occured",c,"times")
-    else:
+    elif len(_list)==1:
         print("letter",letter,"from the given string {",letter_string,"} occured",c,"time")
+    else:
+        print("There is no such letter as",letter)
     return    
 letter_string ='hhjooldlfslfopdjsvvbdfgjhjjfkjvnaiojfkvlfvklkvjf'
 letter=str(input("enter a letter to find it's frequency of occurence : "))
